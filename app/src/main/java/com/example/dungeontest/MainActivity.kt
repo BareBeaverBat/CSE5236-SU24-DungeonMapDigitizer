f3e5c9e4b978c67dd30464fb31c5f95c0ec4f957
BareBeaverBat <salisbury.100@buckeyemail.osu.edu>
2024-06-04 11:43:20 -0400

    add activity lifecycle logging
    for checkpoint 3

--- app/src/main/java/com/example/dungeontest/MainActivity.kt
+++ app/src/main/java/com/example/dungeontest/MainActivity.kt
@@ -2,66 +2,75 @@ package com.example.dungeontest
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.ui.unit.dp
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.CardDefaults
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.launch
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.dungeontest.ui.theme.DungeonTestTheme
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.dungeontest.SettingsScreen
+import androidx.navigation.compose.rememberNavController
+import com.example.dungeontest.ui.theme.DungeonTestTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.v(TAG, "onCreate() called")
         enableEdgeToEdge()
         setContent {
             DungeonTestTheme {
                 MyScreen()
             }
         }
+        Log.v(TAG, "onCreate() finished")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG, "onDestroy() called")
     }
 }
 
