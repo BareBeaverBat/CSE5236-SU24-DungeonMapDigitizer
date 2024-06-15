6915ab167f3149ba0f5222d052a16e1f2cb3d2f4
swyeoh <44318003+pepsipotty@users.noreply.github.com>
2024-06-15 19:25:18 -0400

    wire up cards to data class and misc fixes to textfield focus

--- app/src/main/java/com/example/dungeontest/Settings.kt
+++ app/src/main/java/com/example/dungeontest/Settings.kt
@@ -1,9 +1,13 @@
 package com.example.dungeontest
 
+import android.Manifest
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,11 +31,17 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 
@@ -53,20 +63,27 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import kotlinx.coroutines.CoroutineScope
-
-@OptIn(ExperimentalMaterial3Api::class)
+import com.example.dungeontest.model.AvailableModels
+import com.example.dungeontest.model.cardInfos
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun SettingsScreen(drawerState: DrawerState, scope: CoroutineScope) {
     val snackbarHostState = remember { SnackbarHostState() }
@@ -88,6 +105,8 @@ fun SettingsScreen(drawerState: DrawerState, scope: CoroutineScope) {
                     IconButton(onClick = {
                         scope.launch {
                             drawerState.open()
+                            keyboardController?.hide()
+                            focusManager.clearFocus()
                         }
                     }) {
                         Icon(Icons.Filled.Menu, contentDescription = "Open nav drawer")
@@ -111,13 +130,14 @@ fun SettingsScreen(drawerState: DrawerState, scope: CoroutineScope) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+
         ) {
             Column(
                 verticalArrangement = Arrangement.spacedBy(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxSize()
             ) {
-                Spacer(modifier = Modifier.height(2.dp))
+                Spacer(modifier = Modifier.width(2.dp))
                 SimpleOutlinedTextFieldSample()
                 Spacer(modifier = Modifier.width(2.dp))
                 if (isLandscape) {
@@ -174,6 +194,7 @@ fun SimpleOutlinedTextFieldSample() {
         )
     }
     val textField = FocusRequester()
+    val focusManager = LocalFocusManager.current
     OutlinedTextField(
 
         value = text,
@@ -189,7 +210,12 @@ fun SimpleOutlinedTextFieldSample() {
         singleLine = true,
         shape = MaterialTheme.shapes.large,
         textStyle = TextStyle(brush = brush),
-        label = { Text("Your OpenAI Key") }
+        label = { Text("Your OpenAI Key") },
+        keyboardActions = KeyboardActions(
+            onDone = {
+                focusManager.clearFocus()
+            }
+        ),
 
     )
 }
