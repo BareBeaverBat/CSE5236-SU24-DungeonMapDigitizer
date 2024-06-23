92bf62584e3ba57ab9a63f0b11a35f18bd7aba67
swyeoh <44318003+pepsipotty@users.noreply.github.com>
2024-06-23 04:17:02 -0400

    added AnimatedBorderCard

--- app/src/main/java/com/example/dungeontest/Settings.kt
+++ app/src/main/java/com/example/dungeontest/Settings.kt
@@ -2,6 +2,12 @@ package com.example.dungeontest
 
 import android.content.res.Configuration
 import android.util.Log
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -26,6 +32,7 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.Composable
@@ -42,8 +49,10 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -52,16 +61,22 @@ import androidx.compose.runtime.saveable.SaverScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.Dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.CoroutineScope
 import com.example.dungeontest.model.cardInfos
@@ -92,13 +107,12 @@ fun SettingsScreen(
     val context = LocalContext.current
     val preferences = SettingsStorage(context)
 
-    // Value for the text field from state
+    // Value for the text field from persistent state(preferences datastore)
     var tokenValue = viewModel.tokenValue.value.text
     var selectedModel = viewModel.selectedModel.value
 
-    // Value for the text field from user input
+    // Value for the text field from user input before hitting save
     val tokenValueInput = rememberSaveable { mutableStateOf<String?>(null) }
-
     val selectedModelInput = rememberSaveable { mutableIntStateOf(-1) }
 
 
@@ -244,22 +258,30 @@ fun TextInputField(tokenValueState: String, tokenValueInput: MutableState<String
 fun ModelCard(id: Int, title: String, description: String, selectedModel: Int, selectedModelInput: MutableIntState) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
-    val outlineColor =
-        if (selectedModelInput.value != -1) //value is initialized but no user input
-            if (selectedModelInput.value == id) MaterialTheme.colorScheme.primary
-            else Color.Gray
+    val referencedSelection =
+        if (selectedModelInput.value != -1) selectedModelInput.value
+        else  selectedModel
+
+    val txtColor =
+        if (referencedSelection == id)
+            MaterialTheme.colorScheme.primary
         else
-            if (selectedModel == id) MaterialTheme.colorScheme.primary
-            else Color.Gray
+            Color.Gray
 
-    OutlinedCard(
+    AnimatedBorderCard(
+        modifier = Modifier
+            .width(320.dp)
+            .height(180.dp)
+            .fillMaxWidth(),
 
-        modifier = Modifier,
-        border = BorderStroke(
-            width = 2.dp,
-            color = outlineColor
-        ),
-
+        shape = RoundedCornerShape(12.dp),
+        borderWidth = if (referencedSelection != id) 1.dp else 2.dp,
+        gradient =
+            if (referencedSelection == id)
+                Brush.linearGradient(listOf(Color.Magenta, Color.Cyan))
+            else
+                Brush.linearGradient(listOf(Color.Gray,Color.Gray)),
+        onCardClick = {}
     ) {
         Column(
             modifier = Modifier
@@ -274,8 +296,8 @@ fun ModelCard(id: Int, title: String, description: String, selectedModel: Int, s
             Row(
                 modifier = Modifier
                     .padding(16.dp)
-                    .width(200.dp)
-                    .height(120.dp)
+                    .width(320.dp)
+                    .height(180.dp)
             ) {
                 Column(
                     modifier = Modifier
@@ -285,7 +307,7 @@ fun ModelCard(id: Int, title: String, description: String, selectedModel: Int, s
 
                     //Title
                     Text(
-                        color = outlineColor,
+                        color = txtColor,
 
                         text = title,
                         style = MaterialTheme.typography.headlineMedium,
@@ -297,7 +319,7 @@ fun ModelCard(id: Int, title: String, description: String, selectedModel: Int, s
 
                     //Description
                     Text(
-                        color = outlineColor,
+                        color = txtColor,
                         text = description,
                         style = MaterialTheme.typography.bodyMedium,
                         modifier = Modifier
@@ -307,6 +329,7 @@ fun ModelCard(id: Int, title: String, description: String, selectedModel: Int, s
 
 
                 }
+                Spacer(modifier = Modifier.width(100.dp))
                 Column(
                     modifier = Modifier
                         .padding(start = 10.dp, end = 10.dp)
@@ -329,6 +352,56 @@ fun ModelCard(id: Int, title: String, description: String, selectedModel: Int, s
 
         }
 
+    }
+}
+
+@Composable
+fun AnimatedBorderCard(
+    modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(size = 0.dp),
+    borderWidth: Dp = 2.dp,
+    gradient: Brush = Brush.sweepGradient(listOf(Color.Gray, Color.White)),
+    animationDuration: Int = 5000,
+    enabled: Boolean = true,
+    onCardClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    val infiniteTransition = rememberInfiniteTransition(label = "Infinite Color Animation")
+    val degrees by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = animationDuration, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "Infinite Colors"
+    )
+
+    Surface(
+        modifier = modifier
+            .clip(shape)
+            .clickable { onCardClick() },
+        shape = shape
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(borderWidth)
+                .drawWithContent {
+                    rotate(degrees = degrees) {
+                        drawCircle(
+                            brush = gradient,
+                            radius = size.width,
+                            blendMode = BlendMode.SrcIn,
+                        )
+                    }
+                    drawContent()
+                },
+            color = MaterialTheme.colorScheme.surface,
+            shape = shape
+        ) {
+            content()
+        }
     }
 }
 
