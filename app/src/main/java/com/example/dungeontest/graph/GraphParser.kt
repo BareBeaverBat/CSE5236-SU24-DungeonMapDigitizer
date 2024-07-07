8655195cbcaa23599848452a35eb695049ed2852
BareBeaverBat <salisbury.100@buckeyemail.osu.edu>
2024-07-07 17:14:40 -0400

    fix dot string creation
    to work as intended with our MapRoom dataclass

--- app/src/main/java/com/example/dungeontest/graph/GraphParser.kt
+++ app/src/main/java/com/example/dungeontest/graph/GraphParser.kt
@@ -2,12 +2,16 @@ package com.example.dungeontest.graph
 
 import org.jgrapht.Graph
 import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.nio.DefaultAttribute
 import org.jgrapht.nio.dot.DOTExporter
 import java.io.StringWriter
 
 class GraphParser {
     fun graphToDot(graph: Graph<MapRoom, DefaultEdge>): String {
-        val exporter = DOTExporter<MapRoom, DefaultEdge>()
+        val exporter = DOTExporter<MapRoom, DefaultEdge> { it.id.toString() }
+        exporter.setVertexAttributeProvider {
+            mapOf("label" to DefaultAttribute.createAttribute(it.label))
+        }
         val writer = StringWriter()
 
         exporter.exportGraph(graph, writer)
